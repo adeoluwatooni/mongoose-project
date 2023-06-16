84f0eab4a8a7fbb9dc64d4ef80149013cb6a23ec
Adeoluwa Idowu <123599093+adeoluwatooni@users.noreply.github.com>
2023-06-16 15:31:53 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
 const mongoose = require('mongoose')
 const { deleteMany } = require('./Person')
 
-
 // Connecting to the mongo database 
 mongoose.connect('mongodb://localhost:27017', {useNewUrlParser:true,useUnifiedTopology:true})
 
